bbc41c589625e8008b6da59fec268d527c673e6d
ugrozhalka <uliana.ivashkova@gmail.com>
2023-12-14 17:33:10 +0300

    Change .dart file

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,6 +1,3 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
-
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
   print(1 + 1);
 }
